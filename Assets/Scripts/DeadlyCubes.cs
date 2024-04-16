8e2cb751809806ff9d6a767f0855a50cb0873378
Sebastian <sebastian.samuelsson@elev.ga.ntig.se>
2024-04-16 09:17:45 +0200

    update

--- Assets/Scripts/DeadlyCubes.cs
+++ Assets/Scripts/DeadlyCubes.cs
@@ -6,6 +6,7 @@ public class DeadlyCubes : MonoBehaviour
 {
     void OnTriggerEnter(Collider other) {
         //Code to kill the player
+        //If the player collides with the trigger, then the player dies
         if (other.gameObject.tag == "Player"){
             GameObject.Destroy(other.gameObject);
         }
--- Assets/Scripts/PlatformSpawner.cs
+++ Assets/Scripts/PlatformSpawner.cs
@@ -9,7 +9,9 @@ public class PlatformSpawner : MonoBehaviour
     void OnTriggerEnter(Collider other) {
         //Spawns a random platform when the player passes the trigger
         if(other.gameObject.tag == "Player") {
-            Instantiate(platforms[Random.Range(0, platforms.Length)], new Vector3(transform.position.x,transform.position.y-2,transform.position.z+10),Quaternion.identity);
+            Vector3 platformPosition = new Vector3(transform.position.x,transform.position.y-2,transform.position.z+10);
+            Instantiate(platforms[Random.Range(0, platforms.Length)], platformPosition, Quaternion.identity);
+           
         }
     }
 }
